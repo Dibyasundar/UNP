1366bd225951a839a5525203f7c3878d58fd3199
Dibyasundar <32008769+Dibyasundar@users.noreply.github.com>
2021-04-17 13:22:22 +0530

    Update ASSIGNMENT005.c

--- ASSIGNMENT005.c
+++ ASSIGNMENT005.c
@@ -1,1 +1,2 @@
-//Write a program where the hosts 1 accepts a string of two numbers sent by host 2 and host 1 replies with the sum of two number. 
+//Write a program where the hosts 1 accepts a string of two numbers 
+//sent by host 2 and host 1 replies with the sum of two number. 
