77b9dbb16a8bf8295e522257230bd59d397468d9
Dibyasundar <32008769+Dibyasundar@users.noreply.github.com>
2021-04-24 11:42:51 +0530

    Update ASSIGNMENT005.c

--- ASSIGNMENT005.c
+++ ASSIGNMENT005.c
@@ -4,7 +4,7 @@ sent by host 2 and host 1 replies with the sum of two number.
   
   
   
-The problem is to implement a client - server user-level application using sockets API in C/C++. 
+The problem is to implement a client - server user-level application using sockets API in C. 
 The Server application has to support at least five clients simultaneously. Server accepts strings 
 from clients (even multiple strings from each client) and replies with reverse strings. For example, 
 when client sends “IITHYD”, Server replies with “DYHTII”. Both server and client(s) have to output both 
