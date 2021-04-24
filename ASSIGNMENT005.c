42c158b8027d8c813f98c17eb45ccca9bdf43b9e
Dibyasundar <32008769+Dibyasundar@users.noreply.github.com>
2021-04-24 11:42:17 +0530

    Update ASSIGNMENT005.c

--- ASSIGNMENT005.c
+++ ASSIGNMENT005.c
@@ -1,2 +1,11 @@
 Write a program where the hosts 1 accepts a string of two numbers 
 sent by host 2 and host 1 replies with the sum of two number. 
+
+  
+  
+  
+The problem is to implement a client - server user-level application using sockets API in C/C++. 
+The Server application has to support at least five clients simultaneously. Server accepts strings 
+from clients (even multiple strings from each client) and replies with reverse strings. For example, 
+when client sends “IITHYD”, Server replies with “DYHTII”. Both server and client(s) have to output both 
+sending & receiving strings on the terminal. 
