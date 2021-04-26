9f9aed26044737abd2614ba1a7017d70b1890527
Dibyasundar <32008769+Dibyasundar@users.noreply.github.com>
2021-04-26 13:39:48 +0530

    Create ASSIGNMENT006.c

---
+++ ASSIGNMENT006.c
@@ -0,0 +1,5 @@
+The problem is to implement a client - server user-level application using sockets API in C. 
+The Server application has to support at least five clients simultaneously. Server accepts strings 
+from clients (strings until EOF is provided i.e persistent connection) and replies with reverse strings. For example, 
+when client sends “IITHYD”, Server replies with “DYHTII”. Both server and client(s) have to output both 
+sending & receiving strings on the terminal. 
