eca2dd6ce0b144cf51eeb42ec5246e9cb55db03d
Dibyasundar <32008769+Dibyasundar@users.noreply.github.com>
2021-04-28 13:04:17 +0530

    Create Echo_server_client.c

---
+++ Echo_server_client.c
@@ -0,0 +1,85 @@
+/////////Design a echo server and client 
+
+/////////////  Server.c ///////////////////////////////////////
+#include<stdio.h>
+
+#include<sys/socket.h>
+
+#include <arpa/inet.h>
+#include <unistd.h>
+#include<stdlib.h>
+
+
+#include<time.h>
+#include<string.h>
+
+
+int main(int argc, char *argv[])
+{
+    int n;
+    char data[201];
+    //create a socket
+    int net_socket;
+    net_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    //construct a server
+    //where we want to Listen to
+    struct sockaddr_in server_address, client_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(5600);
+    server_address.sin_addr.s_addr = inet_addr(argv[1]);
+
+    bind(net_socket, (struct sockaddr*)&server_address,sizeof(server_address));
+
+    listen(net_socket,10);
+
+    int fd;
+    int c;
+
+    while(1)
+    {
+	    fd=accept(net_socket,(struct sockaddr*)&client_address,&c);
+	    printf("Accepted\n");
+      n=recv(fd, data,200,0);
+	    write(fd,data,strlen(data));
+    }
+
+    return(0);
+}
+
+
+
+/////////////////////////////////// client.c 
+
+#include<stdio.h>
+
+#include<sys/socket.h>
+
+#include <arpa/inet.h>
+#include <unistd.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+    int n;
+    char data[201];
+    //create a socket
+    int net_socket;
+    net_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    //connect to a server
+    //where we want to connect to
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(5600);
+    server_address.sin_addr.s_addr = inet_addr(argv[1]);
+
+    connect( net_socket, ( struct sockaddr * ) &server_address, sizeof( server_address));
+    fgets(data, 200, stdin);
+    write(net_socket,data,strlen(data));
+    n=recv(net_socket, data,200,0);
+    printf("%s",data);
+    close(net_socket);
+    return(0);
+}
