d475db4096a9269bdd89cb21724382c2921f5d5a
Dibyasundar <32008769+Dibyasundar@users.noreply.github.com>
2021-04-28 11:21:03 +0530

    Update ASSIGNMENT006.c

--- ASSIGNMENT006.c
+++ ASSIGNMENT006.c
@@ -1,3 +1,7 @@
+Write a program to show working of a echo server.
+
+
+
 The problem is to implement a client - server user-level application using sockets API in C. 
 The Server application has to support at least five clients simultaneously. Server accepts strings 
 from clients (strings until EOF is provided i.e persistent connection) and replies with reverse strings. For example, 
