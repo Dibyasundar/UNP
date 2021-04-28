61e668761a99f816c863aaf803d3cef663b6a341
Dibyasundar <32008769+Dibyasundar@users.noreply.github.com>
2021-04-28 11:22:09 +0530

    Update ASSIGNMENT006.c

--- ASSIGNMENT006.c
+++ ASSIGNMENT006.c
@@ -1,3 +1,9 @@
+Write a program where the hosts 1 accepts a string of two numbers 
+sent by host 2 and host 1 replies with the sum of two number. 
+
+
+
+
 Write a program to show working of a echo server.
 
 
