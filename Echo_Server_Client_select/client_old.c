20ec0a0cdb5de542082a8b9da8b58d673f028efc
Dibyasundar <32008769+Dibyasundar@users.noreply.github.com>
2021-05-01 10:25:15 +0530

    Add files via upload

---
+++ Echo_Server_Client_select/client_old.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+#include<sys/socket.h>
+
+#include <arpa/inet.h>
+#include <unistd.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+    int n;
+    char *send_data, *recv_data;
+    //create a socket
+    int net_socket;
+    net_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    //connect to a server
+    //where we want to connect to
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(5600);
+    server_address.sin_addr.s_addr = inet_addr(argv[1]);
+
+    connect( net_socket, ( struct sockaddr * ) &server_address, sizeof( server_address));
+    send_data = (char *) malloc (200*sizeof(char));
+    recv_data = (char *) malloc (200*sizeof(char));
+    while (fgets(send_data, 200, stdin) !=NULL)
+    {
+      write(net_socket,send_data,strlen(send_data));
+      n=recv(net_socket, recv_data,200,0);
+      printf("%s",recv_data);
+      bzero(send_data,200);
+    }
+    free(send_data);
+    free(recv_data);
+    close(net_socket);
+    return(0);
+}
